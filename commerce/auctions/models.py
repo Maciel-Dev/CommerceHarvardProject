6286d28ae6d8aacc24c9e26d0706213d1a0ab4cf
Maciel-Dev <jvmacielv@gmail.com>
2022-05-03 10:30:20 -0300

    Modeling the actual database

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,28 +5,28 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class AuctionListing():
-    pass
-    #ID (Pk)
-    #Title
-    #Description
-    #Start Bid
-    #OPtional - Image URl
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1024)
+    startBid = models.IntegerField(max_length=99999)
+    imageUrl = models.URLField(max_length=2048, blank=True, null=True)
+    
     #Um ou mais usuário tem várias listagens
     
 
-class Bids():
-    pass
-    #ID (PK)
-    #Bid Value
+class Bids(models.Model):
+    bidValue = models.DecimalField(max_digits=6, decimal_places=2)
+    userName = models.ForeignKey(User)
+    dateTime = models.DateTimeField(auto_now_add=True)
+
     #One bid replace the last, if the actual is greater (Comparison)
-    #UserName - FK
-    #Datatime
+
     #Um ou mais usuários pode ter mais de uma Bid
 
 class Comments():
     pass
     #ID (PK)
-    #Comment
-    #UserName - FK
+    comment = models.TextField(max_length=1024)
+    userName = models.ForeignKey(User)
+    #Possibilidade de colocar datetime
     #Um ou mais usuários pode ter mais de um Comment
