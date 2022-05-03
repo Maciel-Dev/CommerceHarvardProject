a20bfc54aadc96b5da11b74cb07ee8fbabba842b
Maciel-Dev <jvmacielv@gmail.com>
2022-05-03 10:03:11 -0300

    Add description to model DB

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing():
+    pass
+    #ID (Pk)
+    #Title
+    #Description
+    #Start Bid
+    #OPtional - Image URl
+    
+
+class Bids():
+    pass
+    #ID (PK)
+    #Bid Value
+    #One bid replace the last, if the actual is greater (Comparison)
+    #UserName - FK
+    #Datatime
+
+class Comments():
+    pass
+    #ID (PK)
+    #Comment
+    #UserName - FK
