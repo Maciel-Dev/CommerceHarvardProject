640f365eb8f52322812d6345dab852b35136c3b1
Maciel-Dev <jvmacielv@gmail.com>
2022-05-03 11:00:35 -0300

    Rewriting Foreign Keys, Working with nulls

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -8,15 +8,15 @@ class User(AbstractUser):
 class AuctionListing(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=1024)
-    startBid = models.IntegerField(max_length=99999)
-    imageUrl = models.URLField(max_length=2048, blank=True, null=True)
+    startBid = models.IntegerField(max_length=9999)
+    imageUrl = models.URLField(blank=True, null=True)
     
     #Um ou mais usuário tem várias listagens
     
 
 class Bids(models.Model):
     bidValue = models.DecimalField(max_digits=6, decimal_places=2)
-    userName = models.ForeignKey(User)
+    userName = models.ForeignKey(User, on_delete=models.RESTRICT)
     dateTime = models.DateTimeField(auto_now_add=True)
 
     #One bid replace the last, if the actual is greater (Comparison)
@@ -24,9 +24,8 @@ class Bids(models.Model):
     #Um ou mais usuários pode ter mais de uma Bid
 
 class Comments():
-    pass
     #ID (PK)
     comment = models.TextField(max_length=1024)
-    userName = models.ForeignKey(User)
+    userName = models.ForeignKey(User, on_delete=models.RESTRICT)
     #Possibilidade de colocar datetime
     #Um ou mais usuários pode ter mais de um Comment
